526bda4b83ad97ba033ef76b4936838944d0be76
Tom Smith <tomgsmith99@gmail.com>
2019-08-29 19:05:20 -0400

    update About link

--- views/view_players_by_year.php
+++ views/view_players_by_year.php
@@ -150,15 +150,3 @@ $content = str_replace("{{SELF_HREF}}", $self_href, $content);
 /***********************************/
 
 show_page($content, "Players " . $season);
-
-// $page = file_get_contents(HTML_PATH . "/page.html");
-
-// $page = str_replace("{{TITLE}}", "Diffendorf Baseball: Players $season", $page);
-
-// $page = str_replace("{{CONTENT}}", $content, $page);
-
-// $page = str_replace("{{web_home}}", WEB_HOME, $page);
-
-// echo $page;
-
-// exit;
