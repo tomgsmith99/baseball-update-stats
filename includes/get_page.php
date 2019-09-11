e4899738b40d6c9c7f327d569c2a4afd8ff7d2e8
Tom Smith <tomgsmith99@gmail.com>
2019-09-11 08:32:44 -0400

    update home link

--- includes/get_page.php
+++ includes/get_page.php
@@ -4,6 +4,8 @@ function get_page($content, $title) {
 
 	$seasons = get_seasons();
 
+	$home = get_home();
+
 	$page = file_get_contents(HTML_PATH . "/page.html");
 
 	$page = str_replace("{{seasons}}", $seasons, $page);
@@ -12,11 +14,22 @@ function get_page($content, $title) {
 
 	$page = str_replace("{{CONTENT}}", $content, $page);
 
+	$page = str_replace("{{home}}", $home, $page);
+
 	$page = str_replace("{{web_home}}", WEB_HOME, $page);
 
 	$page = str_replace("{{views}}", VIEWS, $page);
 
 	return $page;
+}
+
+function get_home() {
+	if (WEB_HOME == "") {
+		return "/";
+	}
+	else {
+		return WEB_HOME;
+	}
 }
 
 function get_seasons() {
